5c5f903d6d7564620341c210e48f201340c55e96
Lukáš Kaprál <80596523+FITlifeSADGE@users.noreply.github.com>
2023-03-01 13:53:05 +0100

    First draft of argparse

---
+++ parse.py
@@ -0,0 +1,21 @@
+import argparse
+import sys
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Supported modes - crack, gen. For more details enter crack -h or gen -h')
+    parser.add_argument('mode', metavar='mode', type=str, choices=['crack', 'gen'], help='Select a function - crack/gen')
+    if len(sys.argv) < 2:
+        print('Plese select a mode - options: crack, gen')
+        exit(1)
+    if sys.argv[1] == 'crack':
+        parser.add_argument('hash', metavar='hash', type=str, help='Enter the hash you want to crack')
+    elif sys.argv[1] == 'gen':
+        parser.add_argument('length', metavar='length', type=int, help='Enter the max length of plaintext password')
+        parser.add_argument('columns', metavar='columns', type=int, help='Enter the length of a chain')
+        parser.add_argument('rows', metavar='rows', type=int, help='Enter the amount of rows')
+        parser.add_argument('restrictions', metavar='restrictions', type=str, choices=['lowercase', 'uppercase', 'lettters', 'special', 'alphanum', 'all'] , help='Enter password restrictions - lowercase, uppercase, lettters, special, alphanum, all')
+        parser.add_argument('algorithm', metavar='algorithm', type=str, help='Select a hashing algorihm')
+
+    args = parser.parse_args()
+    return args
+
